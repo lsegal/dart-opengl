dc04cf34770416c94eae95cf926c455bb41456ac
root.ext <root.ext@gmail.com>
2019-06-09 12:48:46 +0300

    add glfw example

--- example/example.dart
+++ example/example.dart
@@ -1,15 +1,18 @@
+import 'package:opengl/opengl.dart';
 import 'package:opengl/src/opengl_init.dart';
 
 void main() {
   // create OpenGL context and make it current
   //...
 
-  // load OpenGL dynamic library and init all OpenGL functions
+  // load OpenGL dynamic library and init all its functions
   initOpenGL();
 
   // use OpenGL
   //...
+  var result = glGetError();
 
-  // glGetError must return GL_INVALID_OPERATION = 0x0502 without context
-  print(glGetError());
+  // glGetError returns GL_NO_ERROR if successful, 
+  // or GL_INVALID_OPERATION = 0x0502 (1282) without context in our case
+  print('glGetError() result is ${result == GL_NO_ERROR ? "GL_NO_ERROR" : result}');
 }
---
+++ example/example_glfw.dart
@@ -0,0 +1,31 @@
+import 'package:opengl/opengl.dart';
+import 'package:opengl/src/opengl_init.dart';
+import 'package:opengl/src/c_utils.dart';
+import 'package:glfw/src/glfw_init.dart';
+
+void main() {
+  // create OpenGL context and make it current
+  initGlfw();
+
+  glfwInit();
+  print('GLFW: ${CString.fromPointer(glfwGetVersionString()).toUtf8()}');
+  var window = glfwCreateWindow(600, 400, CString.fromUtf8('Dart FFI + GLFW + OpenGL'), null, null);
+  glfwMakeContextCurrent(window);
+
+  // load OpenGL dynamic library and init all its functions
+  initOpenGL();
+  print("GL_VENDOR: ${CString.fromPointer(glGetString(GL_VENDOR))}");
+  print("GL_RENDERER: ${CString.fromPointer(glGetString(GL_RENDERER))}");
+  print("GL_VERSION: ${CString.fromPointer(glGetString(GL_VERSION))}");
+  
+  glClearColor(0.0, 0.7, 0.99, 0.0);
+  glViewport(0, 0, 600, 400);
+
+  while (glfwWindowShouldClose(window) != 1)
+  {
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+  }
+}
