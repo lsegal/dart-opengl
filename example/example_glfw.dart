e5d735d3146e9d4e59f438b7b548372dae33a011
root ext <root.ext@gmail.com>
2019-07-28 09:50:52 +0300

    fix example

--- example/example_glfw.dart
+++ example/example_glfw.dart
@@ -5,29 +5,26 @@ import 'package:opengl/src/c_utils.dart';
 import 'package:glfw/src/glfw_init.dart';
 
 void main() {
-  // create OpenGL context and make it current
   initGlfw();
 
   glfwInit();
-  print('GLFW: ${CString.fromPointer(glfwGetVersionString()).toString()}');
+  print('GLFW: ${glfwGetVersionString().cast<CString>().toString()}');
 
   var monitor = glfwGetPrimaryMonitor();
-  var vm = glfwGetVideoMode(monitor).cast<GLFWvidmode>();
-  var name = glfwGetMonitorName(monitor).cast<CString>().toString();
-  print(name);
-
+  var monitorName = glfwGetMonitorName(monitor).cast<CString>().toString();
+  var videomode = glfwGetVideoMode(monitor).cast<GLFWvidmode>();
+  print('$monitorName ${videomode.width}x${videomode.height}');
+    
   bool isFullscreen = false;
-  glfwWindowHint(GLFW_RED_BITS, vm.redBits);
-  glfwWindowHint(GLFW_GREEN_BITS, vm.greenBits);
-  glfwWindowHint(GLFW_BLUE_BITS, vm.blueBits);
-  glfwWindowHint(GLFW_REFRESH_RATE, vm.refreshRate);
-  //glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-  //glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
-  glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
-  
-
-  var window = glfwCreateWindow(isFullscreen ? vm.width : 480, 
-    isFullscreen ? vm.height : 480, CString.fromString('Dart FFI + GLFW + OpenGL'), 
+  glfwWindowHint(GLFW_RED_BITS, videomode.redBits);
+  glfwWindowHint(GLFW_GREEN_BITS, videomode.greenBits);
+  glfwWindowHint(GLFW_BLUE_BITS, videomode.blueBits);
+  glfwWindowHint(GLFW_REFRESH_RATE, videomode.refreshRate);
+      
+  var window = glfwCreateWindow(
+    isFullscreen ? videomode.width : 640, 
+    isFullscreen ? videomode.height : 480, 
+    CString.fromString('Dart FFI + GLFW + OpenGL'), 
     isFullscreen ? monitor : null, null);
   if (isFullscreen) glfwSetWindowPos(window, 0, 0); 
   glfwMakeContextCurrent(window);
@@ -38,7 +35,7 @@ void main() {
   print("GL_RENDERER: ${CString.fromPointer(glGetString(GL_RENDERER))}");
   print("GL_VERSION: ${CString.fromPointer(glGetString(GL_VERSION))}");
   
-  glClearColor(0.1, 0.1, 0.1, 0.4);
+  glClearColor(0.0, 0.7, 0.99, 0.0);
   glViewport(0, 0, 600, 400);
 
   while (glfwWindowShouldClose(window) != GLFW_TRUE)
